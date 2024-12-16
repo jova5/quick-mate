5bd9f896d05f235727736ab39c5798b6882f43f0
Miloš Jovičić <jovicicm55@outlook.com>
2024-12-16 18:47:20 +0100

    implemented datetime picker

--- app/posts/index.tsx
+++ app/posts/index.tsx
@@ -21,102 +21,121 @@ const NewPost = () => {
   const [cityId, setCityId] = useState<number>(0);
 
   const [time, setTime] = useState(null);
-  const [showTimePicker, setShowTimePicker] = useState(false);
-  const handleTimeChane = (event, selectedTime) => {
-    if (event.type === 'set') {
-      console.log(selectedTime);
-      console.log(selectedTime.split('T'));
-      console.log(selectedTime.split('T')[1]);
-      setTime(selectedTime);
-    }
-    setShowTimePicker(false);
-  }
-
   const [date, setDate] = useState(null);
-  const [showDatePicker, setShowDatePicker] = useState(false);
-  const handleDateChane = (event, selectedDate) => {
+  const [showTime, setShowTime] = useState<string | null>(null);
+  const [showDate, setShowDate] = useState<string | null>(null);
+  const [dateTimeMode, setDateTimeMode] = useState('');
+  const [showTimePicker, setShowTimePicker] = useState(false);
+  const handleTimeChane = (event: any, selectedValue: any) => {
+
     if (event.type === 'set') {
-      console.log(selectedDate);
-      setDate(selectedDate)
+      if (dateTimeMode === 'time') {
+        const hours = String(selectedValue.getHours()).padStart(2, '0'); // Ensures two digits
+        const minutes = String(selectedValue.getMinutes()).padStart(2, '0');
+
+        const time = `${hours}:${minutes}`;
+        setShowTime(time);
+        setTime(selectedValue);
+      } else {
+
+        const day = String(selectedValue.getDate()).padStart(2, '0'); // Ensures two digits
+        const month = String(selectedValue.getMonth() + 1).padStart(2, '0'); // Month is zero-based, so add 1
+        const year = selectedValue.getFullYear(); // Full year (e.g., 2024)
+
+        const formattedDate = `${day}.${month}.${year}`;
+
+        setShowDate(formattedDate);
+        setDate(selectedValue)
+      }
     }
-    setShowDatePicker(false);
+
+    setShowTimePicker(false);
   }
 
   return (
       <>
-      <ScrollView keyboardShouldPersistTaps="handled" style={styles.container}>
-        <TextInput
-            mode="outlined"
-            label="Title"
-            value={title}
-            onChangeText={text => setTitle(text)}
-        />
-        <TextInput
-            mode="outlined"
-            label="Description"
-            value={description}
-            multiline={true}
-            style={{height: 160}}
-            onChangeText={text => setDescription(text)}
-        />
-        <TextInput
-            mode="outlined"
-            label="Contact phone"
-            placeholder="06x123456"
-            value={contactNumber}
-            onChangeText={text => setContactNumber(text)}
-        />
-        <Button mode='outlined' onPress={() => {
-          router.push('/city', {})
-        }}>City</Button>
+        <ScrollView keyboardShouldPersistTaps="handled" style={styles.container}>
+          <TextInput
+              mode="outlined"
+              label="Title"
+              value={title}
+              onChangeText={text => setTitle(text)}
+          />
+          <TextInput
+              mode="outlined"
+              label="Description"
+              value={description}
+              multiline={true}
+              style={{height: 160}}
+              onChangeText={text => setDescription(text)}
+          />
+          <TextInput
+              mode="outlined"
+              label="Contact phone"
+              placeholder="06x123456"
+              value={contactNumber}
+              onChangeText={text => setContactNumber(text)}
+          />
+          <Button mode='outlined' onPress={() => {
+            router.push('/city', {})
+          }}>City</Button>
 
-        <TouchableWithoutFeedback onPress={() => {
-          setShowTimePicker(true)
-        }}>
-          <View style={{
-            height: 52,
-            borderWidth: 1,
-            borderRadius: 4,
-            padding: 12,
-            justifyContent: 'center'
+          <TouchableWithoutFeedback onPress={() => {
+            setDateTimeMode('time')
+            setShowTimePicker(true)
           }}>
+            <View style={{
+              height: 52,
+              borderWidth: 1,
+              borderRadius: 4,
+              borderColor: theme.colors.outline,
+              padding: 12,
+              justifyContent: 'center'
+            }}>
 
-            <Text>Time</Text>
-          </View>
-        </TouchableWithoutFeedback>
+              {
+                showTime ? (
+                    <Text variant="bodyLarge" style={{color: theme.colors.onBackground}}>{showTime}</Text>
 
-        <Button
-            mode='outlined'
-            onPress={() => {
-          setShowTimePicker(true)
-        }}>Time</Button>
+                ): (
+                    <Text variant="bodyLarge" style={{color: theme.colors.onSurfaceVariant}}>11:12</Text>
+                )
+              }
+            </View>
+          </TouchableWithoutFeedback>
 
+          <TouchableWithoutFeedback onPress={() => {
+            setDateTimeMode('date')
+            setShowTimePicker(true)
+          }}>
+            <View style={{
+              height: 52,
+              borderWidth: 1,
+              borderRadius: 4,
+              borderColor: theme.colors.outline,
+              padding: 12,
+              justifyContent: 'center'
+            }}>
+              {
+                showDate ? (
+                    <Text variant="bodyLarge" style={{color: theme.colors.onBackground}}>{showDate}</Text>
 
-        <Button mode='outlined' onPress={() => {
-          setShowDatePicker(true);
-        }}>Date</Button>
-      </ScrollView>
+                ): (
+                    <Text variant="bodyLarge" style={{color: theme.colors.onSurfaceVariant}}>01.01.20XX</Text>
+                )
+              }
+            </View>
+          </TouchableWithoutFeedback>
+        </ScrollView>
         {
             showTimePicker && (
                 <DateTimePicker
+                    display='default'
                     is24Hour={true}
                     timeZoneName={"Europe/Sarajevo"}
-                    mode="time"
-                    value={time || new Date()}
+                    mode={dateTimeMode}
+                    value={dateTimeMode === 'time' ? (time ?? new Date()) : (date ?? new Date())}
                     onChange={handleTimeChane}
-                    negativeButton={{label: "cancel"}}
-                    positiveButton={{label: "ok"}}
-                />
-            )
-        }
-        {
-            showDatePicker && (
-                <DateTimePicker
-                    display='calendar'
-                    timeZoneName={"Europe/Sarajevo"}
-                    mode="date"
-                    value={date || new Date()}
-                    onChange={handleDateChane}
                     negativeButton={{label: "cancel"}}
                     positiveButton={{label: "ok"}}
                 />
